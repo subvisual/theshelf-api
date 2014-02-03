4ed74298836a0e065979ba1e89ccf0f52bbad493
Bruno Azevedo <azevedo.252@gmail.com>
2014-02-03 19:08:35 +0000

    Redesigned book page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -87,7 +87,7 @@ class BooksController < ApplicationController
   private
 
   def book
-    @_book ||= Book.find(params[:id])
+    @_book ||= Book.includes({reviews: :reviewer}).find(params[:id])
   end
 
   def book_params

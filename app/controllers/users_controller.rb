70be00cfd5989fee60c98b7da88acda5b1403214
João Justo <joao@jp-pc.(none)>
2014-08-22 17:15:54 +0100

    Clearance sing_up route hack https://github.com/thoughtbot/clearance/issues/415

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < Clearance::UsersController
-  before_action :handle_disabled_signup, only: [:new, :create]
-
-  def create
-  end
-
+  layout 'simple'
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -16,9 +12,5 @@ class UsersController < Clearance::UsersController
 
   def user_params
     params.require(:user).permit(:password, :first_name, :last_name, :avatar, :avatar_cache, :remove_avatar)
-  end
-
-  def handle_disabled_signup
-    redirect_to log_in_path, notice: t('flashes.signup_disabled')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,6 @@ TheShelf::Application.routes.draw do
 
   get '/log_in' => 'sessions#new', :as => 'log_in'
   delete '/log_out' => 'sessions#destroy', :as => 'log_out'
+  
+  get '/sign_up' => 'users#new', :as => nil
 end

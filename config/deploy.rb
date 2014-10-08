8ef5d9da8dc3a6c090590549b0f2ac1083ddd797
Bruno Azevedo <azevedo.252@gmail.com>
2014-10-08 14:37:53 +0100

    Added .env to Capistrano's list of linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :format, :pretty
 # set :log_level, :debug
 set :pty, true
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .env}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 set :keep_releases, 3
